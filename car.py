0873c3d39b49ccf0652f7d244ff66a0cc12c03b3
Dr_Huitzil <gotenkscoolness300@gmail.com>
2023-07-18 13:11:46 -0700

    modified:   car.py

--- car.py
+++ car.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 class Car(ABC):
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
+        self.engine = engine
+        self.battery = battery
 
     @abstractmethod
     def needs_service(self):
